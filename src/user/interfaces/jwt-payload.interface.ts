fa283fab7bf0595b8af2f46b38bef22dd75f1b35
chunjiu <1614949142@qq.com>
2026-01-26 23:16:23 +0800

    refactor: optimize token generation in user controller

--- src/user/interfaces/jwt-payload.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface JwtPayload {
-  userId: number;
-  username?: string;
-  roles?: string[];
-  permissions?: string[];
-}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -31,37 +31,40 @@ export class UserController {
   @Post('login')
   async userLogin(@Body() loginUser: LoginUserDto) {
     const vo = await this.userService.login(loginUser, false);
-
-    vo.accessToken = this.jwtService.sign(
-      {
-        userId: vo.userInfo.id,
-        username: vo.userInfo.username,
-        roles: vo.userInfo.roles,
-        permissions: vo.userInfo.permissions,
-      },
-      {
-        expiresIn:
-          this.configService.get('jwt_access_token_expires_time') || '30m',
-      },
-    );
-
-    vo.refreshToken = this.jwtService.sign(
-      {
-        userId: vo.userInfo.id,
-      },
-      {
-        expiresIn:
-          this.configService.get('jwt_refresh_token_expres_time') || '7d',
-      },
-    );
-
-    return vo;
+    return this.generateTokens(vo);
   }
 
   @Post('admin/login')
   async adminLogin(@Body() loginUser: LoginUserDto) {
     const vo = await this.userService.login(loginUser, true);
+    return this.generateTokens(vo);
+  }
 
+  @Get('refresh')
+  async refresh(@Query('refreshToken') refreshToken: string) {
+    try {
+      const data = this.jwtService.verify(refreshToken);
+      const user = await this.userService.findUserById(data.userId, false);
+      return this.generateTokens({ userInfo: user } as any);
+    } catch (e) {
+      console.error(e);
+      throw new UnauthorizedException('token 已失效，请重新登录');
+    }
+  }
+
+  @Get('admin/refresh')
+  async adminRefresh(@Query('refreshToken') refreshToken: string) {
+    try {
+      const data = this.jwtService.verify(refreshToken);
+      const user = await this.userService.findUserById(data.userId, true);
+      return this.generateTokens({ userInfo: user } as any);
+    } catch (e) {
+      console.error(e);
+      throw new UnauthorizedException('token 已失效，请重新登录');
+    }
+  }
+
+  private generateTokens(vo: any) {
     vo.accessToken = this.jwtService.sign(
       {
         userId: vo.userInfo.id,
@@ -85,84 +88,10 @@ export class UserController {
       },
     );
 
-    return vo;
-  }
-  @Get('refresh')
-  async refresh(@Query('refreshToken') refreshToken: string) {
-    try {
-      const data = this.jwtService.verify(refreshToken);
-
-      const user = await this.userService.findUserById(data.userId, false);
-
-      const access_token = this.jwtService.sign(
-        {
-          userId: user.id,
-          username: user.username,
-          roles: user.roles,
-          permissions: user.permissions,
-        },
-        {
-          expiresIn:
-            this.configService.get('jwt_access_token_expires_time') || '30m',
-        },
-      );
-
-      const refresh_token = this.jwtService.sign(
-        {
-          userId: user.id,
-        },
-        {
-          expiresIn:
-            this.configService.get('jwt_refresh_token_expres_time') || '7d',
-        },
-      );
-
-      return {
-        access_token,
-        refresh_token,
-      };
-    } catch (e) {
-      console.error(e);
-      throw new UnauthorizedException('token 已失效，请重新登录');
-    }
-  }
-  @Get('admin/refresh')
-  async adminRefresh(@Query('refreshToken') refreshToken: string) {
-    try {
-      const data = this.jwtService.verify(refreshToken);
-
-      const user = await this.userService.findUserById(data.userId, true);
-
-      const access_token = this.jwtService.sign(
-        {
-          userId: user.id,
-          username: user.username,
-          roles: user.roles,
-          permissions: user.permissions,
-        },
-        {
-          expiresIn:
-            this.configService.get('jwt_access_token_expires_time') || '30m',
-        },
-      );
-
-      const refresh_token = this.jwtService.sign(
-        {
-          userId: user.id,
-        },
-        {
-          expiresIn:
-            this.configService.get('jwt_refresh_token_expres_time') || '7d',
-        },
-      );
-
-      return {
-        access_token,
-        refresh_token,
-      };
-    } catch (e) {
-      console.error(e);
-      throw new UnauthorizedException('token 已失效，请重新登录');
-    }
+    return {
+      accessToken: vo.accessToken,
+      refreshToken: vo.refreshToken,
+      userInfo: vo.userInfo,
+    };
   }
 }
